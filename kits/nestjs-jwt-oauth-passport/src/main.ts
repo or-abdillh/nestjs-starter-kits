d49ae3abc159efc052b7e151a79f90ac0e81b228
or-abdillh <hans.abdillh05@gmail.com>
2026-01-05 10:47:31 +0800

    feat: setup Swagger

--- kits/nestjs-jwt-oauth-passport/src/main.ts
+++ kits/nestjs-jwt-oauth-passport/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
+
+  // Create NestJS app
   const app = await NestFactory.create(AppModule);
+
+  // Enable versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('NestJS JWT OAuth Passport Starter Kit')
+    .setDescription('Authentication API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  // Create Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup Swagger
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
